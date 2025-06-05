aea10d9f9d2ba23a8aaf7292b5f33af81745bedb
vkilzold <princemaynardmasilac@gmail.com>
2025-06-05 15:53:15 +0800

    revert to using png

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,3 +1,48 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+// Disable smoothing for pixelated effect
+ctx.imageSmoothingEnabled = false;
+
+const spriteSheet = new Image();
+spriteSheet.src = 'animations/player-idle.png';
+
+const frameWidth = 32;
+const frameHeight = 32;
+const totalFrames = 16;
+let currentFrame = 0;
+let lastTime = 0;
+
+const scale = 4;
+const fps = 12;
+const frameTime = 1000 / fps;
+
+spriteSheet.onload = () => {
+  requestAnimationFrame(draw);
+};
+
+function draw(timestamp) {
+  if (timestamp - lastTime > frameTime) {
+    currentFrame = (currentFrame + 1) % totalFrames;
+    lastTime = timestamp;
+  }
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.drawImage(
+    spriteSheet,
+    currentFrame * frameWidth, 0,
+    frameWidth, frameHeight,
+    100, 100,
+    frameWidth * scale, frameHeight * scale
+  );
+
+  requestAnimationFrame(draw);
+}
+
+
+
+
 function changeGIF(gifPath) {
   const img = document.getElementById("animation-sprite");
   
@@ -7,7 +52,7 @@ function changeGIF(gifPath) {
   // If it's not the idle animation, return to idle after 1 second
   if (!gifPath.includes('idle')) {
     setTimeout(() => {
-      img.src = 'animations/char-idle.gif?t=' + new Date().getTime();
+      img.src = 'animations/player-idle.gif?t=' + new Date().getTime();
     }, 1000); // 1000 milliseconds = 1 second
   }
 }
